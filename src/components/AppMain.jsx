ce284dcf0298d6a3803a197c0ff691b9e7e994c6
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-03-17 16:18:02 +0100

    add dynamic title and alphabetic order for th actors card

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -7,11 +7,17 @@ export default function AppMain() {
     //API
     const maleActorsApi = 'https://lanciweb.github.io/demo/api/actors/' //Salvo il mio endpoint in una variabile
     const famaleActorsApi = 'https://lanciweb.github.io/demo/api/actresses/' //Salvo il mio endpoint in una variabile
+
+    //USESTATES
     const [render, setRender] = useState([]) //Variabile che renderizerrà i miei dati nella UI.
     const [maleActorsList, setMaleActorsList] = useState([]) //Creo una variabile di stato per rendere dinamici e salvare esternamente i dati della mia API
     const [femaleActorsList, setFemaleActorsList] = useState([]) //Creo una variabile di stato per rendere dinamici e salvare esternamente i dati della mia API
-    const [completeList, setCompleteList] = useState([])
-    //useEffects
+    const [completeList, setCompleteList] = useState([]) //Variabile che conterà l'intera lista.
+    const [title, setTitle] = useState('')
+    
+
+
+    //USEEFFECTS
 
     useEffect(() => { //Uso use effect per effeuare una chiamata HTTP all'avvio della pagaina
 
@@ -56,12 +62,12 @@ export default function AppMain() {
 
         setCompleteList([...maleActorsList, ...femaleActorsList])  //Salvo in una variabile di stato il nuovo array con tutte e due le mie variabili di stato precedentemente aggiornate.
         setRender(completeList) //Imposto la variabile che renderizza gli elementi in pagina uguale alla variabile appena creata.
-
+        setTitle('All')
         console.log('Ho settato la lista di attori completi');
 
     }, [maleActorsList, femaleActorsList])
 
-
+    
     return (
         /* Main */
         <main>
@@ -69,13 +75,13 @@ export default function AppMain() {
             {/* Nav bar */}
             <ul class="nav justify-content-center p-3 bg-dark-subtle">
                 <li class="nav-item">
-                    <button className="nav-link" onClick={() => setRender(completeList)} aria-current="page">All</button>
+                    <button className="nav-link" onClick={() => {setRender(completeList); setTitle('All')}} aria-current="page">All</button>
                 </li>
                 <li class="nav-item">
-                    <button className="nav-link" onClick={() => setRender(maleActorsList)}>Man</button>
+                    <button className="nav-link" onClick={() =>{ setRender(maleActorsList); setTitle('Mans')}}>Mans</button>
                 </li>
                 <li class="nav-item">
-                    <button className="nav-link" onClick={() => setRender(femaleActorsList)}>Woman</button>
+                    <button className="nav-link" onClick={() => {setRender(femaleActorsList); setTitle('Womans')}}>Womans</button>
                 </li>
 
             </ul>
@@ -85,12 +91,14 @@ export default function AppMain() {
                 
                 {/* Cards section */}
                 <section className="mt-5">
+                    <h2 className="text-white text-center fs-1 my-5">{title.toUpperCase()}</h2>
                     <div className="container">
                         {/* Row */}
                         <div className="row justify-content-evenly row-gap-4">
 
-                            {render.map((actor, index) => (  //Utilizzo il map per ciclare la mia varaibile di stato e renderizzo il markup in
+                            {render.sort((a, b) => a.name.localeCompare(b.name)).map((actor, index) => (  //Utilizzo il map per ciclare la mia varaibile di stato e renderizzo il markup in
 
+                                
                                 /* Cards/Row */
                                 <div key={index} className="card col-12 col-sm-6 col-md-4 col-lg-3 card-bgc" style={{ width: '18rem' }}>
 
@@ -105,7 +113,10 @@ export default function AppMain() {
 
                                     {/* Card Body */}
                                     <div className="card-body">
-                                        <p className="card-text rounded"> {actor.biography}<br /><span>Premi: {actor.awards}</span></p>
+                                        <p className="card-text rounded"> 
+                                            {actor.biography}<br />
+                                            <span className=" d-block my-2">Premi: {actor.awards}</span>
+                                            </p>
                                     </div>
 
                                 </div>
