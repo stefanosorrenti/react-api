1163b17151172a84ef8896a31252496cf3dfb18e
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-03-17 12:16:44 +0100

    milestone 1: add API call at page startup with respective logs

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,5 +1,45 @@
+//IMPORT
+import { useEffect } from "react"
 export default function AppMain() {
 
+    //DATA
+
+    //API
+    const maleActorsApi = 'https://lanciweb.github.io/demo/api/actors/' //Salvo il mio endpoint in una variabile
+    const famaleActorsApi = 'https://lanciweb.github.io/demo/api/actresses/' //Salvo il mio endpoint in una variabile
+
+
+    //useEffects
+
+    useEffect(()=>{ //Uso use effect per effeuare una chiamata HTTP all'avvio della pagaina
+
+        fetch(maleActorsApi) //Effettuo la chiamata
+
+        .then(data => data.json()) //Effetuo il parsing dei dati
+
+        .then(maleActors => { //Utilizzo i dati.
+            console.log('Lista attori:');
+            console.log(maleActors);
+            
+            
+        })
+
+    }, [])
+
+    //FACCIO LO STESSO ESATTO PROCEDIMENTO MA CON L'API DELLE ATTRICI
+
+    useEffect(() => {
+        fetch(famaleActorsApi)
+
+        .then(data => data.json())
+
+        .then(femaleActors => {
+            console.log('Lista attrici');
+            console.log(femaleActors);
+            
+            
+        })
+    }, [])
     return (
         <main>
             PROVA MAIN
